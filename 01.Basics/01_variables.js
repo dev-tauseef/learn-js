c5750da924e930baedd0340b29528bea25c1a264
Mohammed Tauseef <98013032+dev-tauseef@users.noreply.github.com>
2024-08-01 11:24:54 +0000

    variables done!

---
+++ 01.Basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountID = 123456
+let accountName = "ABC"
+let accountEmail = "abc@xyz.com"
+var accountPassword = "Qwerty"
+accountCity = "Delhi"
+
+// accountID = 5 // const value cannot be changed
+
+//console.log(accountID);
+// console.table([accountID,accountName,accountPassword,accountCity])
+
+accountName = "XYZ"
+accountEmail = "xyz@abc.com"
+accountPassword = "ZXCV"
+accountCity = "New Delhi"
+
+console.table ([accountID,accountName,accountEmail,accountPassword,accountCity])
