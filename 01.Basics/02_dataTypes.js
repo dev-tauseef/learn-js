6a83491bef3a747660602957a865a85bfc548bfc
Mohammed Tauseef <98013032+dev-tauseef@users.noreply.github.com>
2024-08-03 19:28:56 +0000

    Data Types

---
+++ 01.Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //To use modern JS standards
+
+//alert( 3 + 3)// it won't work because we are using NodeJS, it will work in browsers (v8 Engines)
+
+let number = 2
+let name = "ABC"
+let state;
+let isLoggedIn = false
+
+/* Primitive DataTypes */
+
+//number => 2 to the power 53 (Number Range)
+//bigint => used for large numbers (to be used on large websites like reddit, facebook etc..)
+//string => ""
+//boolean => true/false
+//null => stand alone value for empty data
+//undefined => when value is not assigned to the variable
+//symbol => to find out unique components (e.g: used in figma components)
+
+
+console.log(typeof isLoggedIn);
