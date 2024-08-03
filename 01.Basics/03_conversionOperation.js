25f265af0b7d054986e80683e1b7b441bfc6b599
Mohammed Tauseef <98013032+dev-tauseef@users.noreply.github.com>
2024-08-03 20:29:17 +0000

    Conversion Incomplete

---
+++ 01.Basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = 7
+let stringscore = "7abc"
+let nullscore = null
+let varscore = undefined
+
+//console.log(score)
+//console.log(typeof score)
+// console.log(typeof (score))
+
+let stringConvert = String(score);
+console.log(typeof stringConvert)
+
+console.log(stringConvert)
+
+let numberConvert = Number(score)
+console.log(typeof numberConvert)
+
+console.log(numberConvert)
+
