04ba8e7cba59842f41df68b2caf9fbb9de2607f8
Mohammed Tauseef <98013032+dev-tauseef@users.noreply.github.com>
2024-08-06 10:45:25 +0000

    Operations Incomplete

--- 01.Basics/03_conversionOperation.js
+++ 01.Basics/03_conversionOperation.js
@@ -54,9 +54,9 @@ let negValue = -value
 // console.log(2+2);
 // console.log(2-2);
 // console.log(2*2);
-// console.log(2**3);
+// console.log(2**3); [Exponent]
 // console.log(2/3);
-// console.log(2%3);
+// console.log(2%3);  [Remainder or Modulus]
 
 let str1 = "hello"
 let str2 = " hitesh"
