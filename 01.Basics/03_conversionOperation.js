a6df68722411a84cdf815f3beced28660010b8db
Mohammed Tauseef <98013032+dev-tauseef@users.noreply.github.com>
2024-08-06 10:15:18 +0000

    Conversion Complete!

--- 01.Basics/03_conversionOperation.js
+++ 01.Basics/03_conversionOperation.js
@@ -1,3 +1,5 @@
+//Number Conversion
+
 let score = 7
 let stringscore = "7abc"
 let nullscore = null
@@ -12,8 +14,32 @@ console.log(typeof stringConvert)
 
 console.log(stringConvert)
 
-let numberConvert = Number(score)
+let numberConvert = Number(stringscore)
 console.log(typeof numberConvert)
 
 console.log(numberConvert)
 
+
+//"33" => 33
+//"33abc" => NaN (Not a Number) [Type of NaN is number]
+//true => 1; false => 0;
+
+
+//Boolean Conversion
+
+let isLoggedIn = 1
+let booleanConvert = Boolean(isLoggedIn);
+console.log(booleanConvert);
+console.log(typeof booleanConvert)
+
+// 1 => true 1; 0 => false;
+// "" => false
+// "abc" => true
+
+
+//String Conversion
+
+let aNumber = 123
+let convertString = String(aNumber)
+console.log(convertString)
+console.log(typeof convertString)
